0eb364d2f8044aef4f48bcfe10b10d73f1ccaf92
Gupta Pandiri <gupta.pandiri@contentstack.com>
2023-04-22 14:55:48 +0530

    feat(hello): adding timeout

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,9 @@
 export default function handler(request, response) {
-    const { name } = process.env;
+  const { name } = process.env;
+  setTimeout(() => {
     response.status(200).json({
-        message: "Hello from the serverless function!",
-        name
+      message: "Hello from the serverless function!",
+      name,
     });
-  }
+  }, 10000);
+}
